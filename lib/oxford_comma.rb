ea3844aa010b38e26692476f84eede0cb22479b3
Seth Todd <seth.todd@outlook.com>
2020-01-26 20:53:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-  array.join(" and ")
+ if array.size < 3
+    array.join(" and ")
+    else
+  new_array = array[0...-1]
+  new_array.join(", ") << ", and #{array.last}"
+end
 end
